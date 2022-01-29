4da570e1631c47411d60568c1ee7e228240343b2
Gilles Migliori <gilles.migliori@gmail.com>
2022-01-29 17:27:11 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# ajax-fetch-data-loader
-Loads the content called with Javascript Fetch() into an HTML page and triggers the scripts without any complications
+
+# Ajax Fetch() Data Loader
+
+This small script allows you to insert HTML content and Javascript code retrieved in Ajax with the Javascript Fetch() API
+
+## Demo
+
+<https://www.ajax-fetch-data-loader.miglisoft.com>
+
+## About
+
+The Fetch() Javascript API is a simple and modern way to load dynamic content into a page.
+Unfortunately if the loaded content contains some Javascript code it will not be interpreted, that is why I provide this script.
+
+## Features
+
+- parse the content retrieved with Fetch
+- creates an Array with the HTML nodes and the scripts at the end
+- parses the Array and:
+  - inserts the html nodes in the destination container
+  - inserts the javascript into &lt;script /&gt; tags, which allows them to be executed.
+
+## Usage/Examples
+
+```javascript
+<script>
+    document.addEventListener('DOMContentLoaded', function(event) {
+        fetch('data.html')
+        .then(function (response) {
+            return response.text()
+        })
+        .then(function (html) {
+            console.info('content has been fetched from data.html');
+            loadData(html, '#ajax-target').then(function (html) {
+                console.info('I\'m a callback');
+            })
+        }).catch((error) => {
+            console.log(error);
+        });
+    });
+</script>
+```
+
+## Authors
+
+- [@migliori](https://www.github.com/migliori)
---
+++ fetch-data-loader.js
@@ -0,0 +1,77 @@
+
+/**
+ * Parse the HTML/Javascript nodes
+ *
+ * @param {string} html the HTML/Javascript returned by fetch()
+ * @return {array} a table with the html nodes then the scripts at the end
+ */
+var _htmlToElements = function (html) {
+    var template = document.createElement('template');
+    template.innerHTML = html;
+
+    const nodes = template.content.childNodes,
+        nodesArray = [],
+        scriptsArray = [];
+    for (var i in nodes) {
+        if (nodes[i].nodeType == 1) { // get rid of the whitespace text nodes
+            if (nodes[i].nodeName === 'SCRIPT') {
+                scriptsArray.push(nodes[i]);
+            } else {
+                nodesArray.push(nodes[i]);
+            }
+        }
+    }
+    return nodesArray.concat(scriptsArray);
+}
+
+/**
+ * recursive function that loads each node into the container and then loads the scripts
+ *
+ * @param {string} data
+ * @param {integer} index
+ * @param {string} container
+ * @return {boolean} true on success after recursion
+ */
+var _loadContent = function (data, index, container) {
+    if (index === 0) {
+        document.querySelector(container).innerHTML = '';
+    }
+    if (index <= data.length) {
+        var element = data[index];
+        if (element !== undefined && element.nodeName === 'SCRIPT') {
+            // output scripts
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            if (element.src != '') {
+                script.src = element.src;
+                script.onload = function () {
+                    _loadContent(data, index + 1, container);
+                };
+                document.head.appendChild(script);
+            } else {
+                script.text = element.text;
+                document.body.appendChild(script);
+                _loadContent(data, index + 1, container);
+            }
+        } else {
+            if (element !== undefined) {
+                document.querySelector(container).appendChild(element);
+            }
+            _loadContent(data, index + 1, container);
+        }
+    } else {
+        return true;
+    }
+};
+
+
+/**
+ * the main function to load the fetched content into the HTML container
+ *
+ * @param {string} data the HTML/Javascript returned by fetch()
+ * @param {string} container the container target selector. ie: '#ajax-target'
+ * @return {boolean} true on success
+ */
+var loadData = async function (data, container) {
+    return _loadContent(_htmlToElements(data), 0, container);
+}
